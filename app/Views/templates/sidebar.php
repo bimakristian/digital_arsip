5503b7be2dd179934b04d75449e25ceb6959b6eb
Bima Kristian <bimachristian10@gmail.com>
2023-05-05 16:35:58 +0700

    first commit

---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,46 @@
+ <!-- Sidebar -->
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+     <!-- Sidebar - Brand -->
+     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+         <div class="sidebar-brand-icon rotate-n-15">
+             <i class="fas fa-file"></i>
+         </div>
+         <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+     </a>
+
+     <!-- Divider -->
+     <hr class="sidebar-divider">
+     <!-- Heading -->
+     <div class="sidebar-heading">
+         User Profile
+     </div>
+     <!-- Nav Item profile -->
+     <li class="nav-item">
+         <a class="nav-link" href="charts.html">
+             <i class="fas fa-user"></i>
+             <span>Profile</span></a>
+     </li>
+
+     <!-- Nav Item - Tables -->
+     <li class="nav-item">
+         <a class="nav-link" href="tables.html">
+             <i class="fas fa-user-edit"></i>
+             <span>Edit Profil</span></a>
+     </li>
+
+
+     <!-- Divider -->
+     <hr class="sidebar-divider d-none d-md-block">
+     <li class="nav-item">
+         <a class="nav-link" href="tables.html">
+             <i class="fas fa-sign-out-alt"></i>
+             <span>Logout</span></a>
+     </li>
+     <!-- Sidebar Toggler (Sidebar) -->
+     <div class="text-center d-none d-md-inline">
+         <button class="rounded-circle border-0" id="sidebarToggle"></button>
+     </div>
+
+ </ul>
+ <!-- End of Sidebar -->
